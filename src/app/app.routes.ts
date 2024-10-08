e8cf6cb9114900ce41bcd848016c8984a6a45fd6
CannyRo <whatcannydev@gmail.com>
2024-10-08 23:50:45 +0200

    Routing initialization with home redirect to dessert and other path to page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DessertComponent } from './page/dessert/dessert.component';
+import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "dessert", component: DessertComponent, title: "Dessert "},
+    { path: "", redirectTo: "/dessert", pathMatch: "full"},
+    { path:"**", component: PageNotFoundComponent, title: "Page not found"}
+];
