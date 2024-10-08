cbf227790db4808ff34ca7adeae4fbca89c61a7f
CannyRo <whatcannydev@gmail.com>
2024-10-08 23:30:20 +0200

    Skeleton of main raw services

---
+++ src/app/service/border-product-card.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBorderProductCard]',
+  standalone: true
+})
+export class BorderProductCardDirective {
+
+  constructor() { }
+
+}
