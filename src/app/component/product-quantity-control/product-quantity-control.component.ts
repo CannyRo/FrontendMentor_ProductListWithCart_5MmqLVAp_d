8beaaf256f13d667ac190e70e4d59a5750868894
CannyRo <whatcannydev@gmail.com>
2024-10-08 23:26:46 +0200

    Skeleton of main raw components

---
+++ src/app/component/product-quantity-control/product-quantity-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-quantity-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-quantity-control.component.html',
+  styleUrl: './product-quantity-control.component.css'
+})
+export class ProductQuantityControlComponent {
+
+}
