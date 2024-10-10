6e525ec73fb1681e0287946df69ba9313abc7906
CannyRo <whatcannydev@gmail.com>
2024-10-10 22:58:24 +0200

    Directive to show or hide border color var(--Red) on product thumbnail which is in the cart

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -21,25 +21,16 @@ export class ProductService {
     return this.cart().reduce((total, cartItem) => total + (cartItem.quantity * cartItem.product.price), 0);
   })
 
-  constructor() {
-    effect(()=> console.log('Cart is updated in ProductService ==> ', this.cart()));
-    
-  }
 
   addProduct(product: Product) {
-    console.log('Add product : ', product);
     this.cart.update( cart => [...cart, {product: product, quantity:1}]);
   }
-
   increaseProduct(product: Product) {
-    console.log('Increase product : ', product);
     this.cart.update( cart => 
       cart.map( cartItem => cartItem.product.name === product.name ?
         { product: product, quantity: cartItem.quantity + 1 } : cartItem ));
   }
-
   decreaseProduct(product: Product) {
-    console.log('Decrease product : ', product);
     this.cart.update( cart => 
       cart.map( cartItem => 
         cartItem.product.name === product.name && cartItem.quantity > 1 
@@ -49,16 +40,11 @@ export class ProductService {
         : cartItem 
       ).filter(cartItem => cartItem !== null));
   }
-
   removeProduct(product: Product) {
-    console.log('Remove product : ', product);
     this.cart.update( cart => cart.filter( cartItem =>
       cartItem.product.name !== product.name));
   }
-
   resetCart(cart: CartItem[]) {
-    console.log('Reset cart', cart);
     this.cart.set([]);
   }
-
 }
